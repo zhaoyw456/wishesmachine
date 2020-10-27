5d721e482b467e3fd42bacf0b410c9e627dbe4c8
LiChao <1172274095@qq.com>
2020-10-27 22:06:12 +0800

    修改日常问候 生日祝福页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,27 @@
+// Page({
+//   home(){
+//     wx.switchTab({
+//       url: '/pages/festival/festival',
+//     })
+//   }
+// })
+//index.js
+//获取应用实例
+const app = getApp()
 Page({
-  home(){
+  data: {
+    motto: '不管你在任何时候，不管你在任何地方，反正你都会知道，总会有那么一个人。',
+    mottoAuthor: '张爱玲',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    author: "张爱玲",
+  },
+  navigateTo() {
     wx.switchTab({
       url: '/pages/festival/festival',
     })
   }
+  
 })
+
