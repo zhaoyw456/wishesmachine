a009e3b7726391be057339c4cfd8e593120ac234
LiChao <1172274095@qq.com>
2020-10-28 17:08:41 +0800

    1

--- pages/everyday/everyday.js
+++ pages/everyday/everyday.js
@@ -5,27 +5,37 @@ Page({
   data:{
     wish:"",  //这是一个空的数组，等下获取到云数据库的数据将存放在其中
     sj:util.formatTime(),
+    goodimg: 'goodmorning'
   },
+  transferImg() {
+
+  },
+  /*
+  
+  */
   onTabItemTap(item) {
     if(util.formatTime() == '早安'){
       everyDB.collection("morning").aggregate().sample({size : 3}).end().then(res =>{
         console.log(res);
         this.setData({
-          wish : res.list
+          wish : res.list,
+          goodimg: 'goodmorning'
         })
       })
     }else if(util.formatTime() == '午安'){
       everyDB.collection("noon").aggregate().sample({size : 3}).end().then(res =>{
         console.log(res);
         this.setData({
-          wish : res.list
+          wish : res.list,
+          goodimg: 'goodnoon'
         })
       })
     }else{
       everyDB.collection("night").aggregate().sample({size : 3}).end().then(res =>{
         console.log(res);
         this.setData({
-          wish : res.list
+          wish : res.list,
+          goodimg:'goodnight'
         })
       })
     }
