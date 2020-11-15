9dec9350acde766215a8adb38672578dc7f2bf52
LiChao <1172274095@qq.com>
2020-11-15 12:31:34 +0800

    生日祝福页面 图片缩略图   图片预览  图片保存 图片转发

--- pages/birthday/birthday.js
+++ pages/birthday/birthday.js
@@ -1,37 +1,54 @@
 // pages/everyday/everyday.js
 const everyDB_text = wx.cloud.database().collection("birthday")
 const everyDB_picture = wx.cloud.database().collection("fileID_birthday")
+let imgList = []
+let firstSwitch = 0//第一次点击选择图片 加载图片
 Page({
   data:{
     wish:"",  //这是一个空的数组，等下获取到云数据库的数据将存放在其中
     picture:'',
     textDisplay : true,
+    //nav
+    TabCur: 0,
+    scrollLeft:0,
+    checked: false,
+    //imgList: []
   },
   onTabItemTap(item) {
     this.updata()
   },
-  text(){
-    this.setData({
-      textDisplay : true
-    })
-  },
-  picture(){
-    this.setData({
-      textDisplay : false
-    })
-  },
+  // text(){
+  //   this.setData({
+  //     textDisplay : true
+  //   })
+  // },
+  // picture(){
+  //   this.setData({
+  //     textDisplay : false
+  //   })
+  // },
   updata(){
-    everyDB_text.aggregate().sample({size : 3}).end().then(res =>{
-      console.log(res);
-      this.setData({
-        wish : res.list
+    if(!this.data.checked) {
+      everyDB_text.aggregate().sample({size : 3}).end().then(res =>{
+  
+        this.setData({
+          wish : res.list
+        })
       })
-    })
-    everyDB_picture.aggregate().sample({size : 1}).end().then(res =>{
-      this.setData({
-        picture : res.list
+    } else {
+      everyDB_picture.aggregate().sample({size : 9}).end().then(res =>{
+        console.log("pic",res)
+        this.setData({
+          picture : res.list
+        })
+        //push imageList数组
+        let i = 0
+        for(;i<res.list.length;i++) {
+          imgList.push(res.list[i].fileID)
+        }
+      
       })
-    })
+    }
   },
   copyWish(e) {
     console.log(e)
@@ -47,5 +64,39 @@ Page({
             })
           }
         })
+  },
+  //导航条
+  tabSelect(e) {
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+    })
+  },
+  //开关选择器bingchange
+  switchCheckStatus(e) {
+
+    this.setData({
+      checked: !this.data.checked 
+    })
+    //只在第一次点击选择图片查询图片
+    if(!firstSwitch) {
+      this.updata()
+      console.log("kaiguan")
+      firstSwitch = 1
+    }
+    
+    
+
+  },
+  //预览图片
+  previewImage(event) {
+   
+    let currentUrl = event.currentTarget.dataset.src
+    //生成urls
+
+
+    wx.previewImage({
+      current: currentUrl, // 当前显示图片的http链接
+      urls: imgList // 需要预览的图片http链接列表
+    })
   }
 })
